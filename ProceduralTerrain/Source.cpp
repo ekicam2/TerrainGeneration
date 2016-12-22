2e3bc5e075cd00497a201e052b51fee0e651cd6f
ekicam2 <ekicam2@gmail.com>
2016-12-22 10:58:20 +0100

    updated todo list

--- ProceduralTerrain/Source.cpp
+++ ProceduralTerrain/Source.cpp
@@ -2,18 +2,20 @@
 
 /*
 TODO:
-1 make a triangle and draw at the screen [ ]
+1 make a triangle and draw at the screen [X]
     1 make 3 vertices [X]
     2 make vertex shader [X]
     3 make fragment shader [X]
     4 compile shaders [X]
     5 link them into a program [X]
     6 draw it on the screen [X]
-2 make perlin noise algorithm [ ]
+2 make a triangle strip and draw at the screen [ ]
+    1 make a grid based on a triangle strip [ ]
+    2 draw it uing as less transformation as possible [ ]
+3 make perlin noise algorithm [ ]
     1 code perlin noise algorith [ ]
     2 draw it to a texture [ ]
     3 read from the texture [ ]
-3 make a triangle strip and draw at the screen [ ]
 
 */
 

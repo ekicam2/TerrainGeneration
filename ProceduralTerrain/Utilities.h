4930d28bfd7b989fe5d8bc117a60df6a9777d0af
ekicam2 <ekicam2@gmail.com>
2017-01-19 23:25:28 +0100

    bugfix

--- ProceduralTerrain/Utilities.h
+++ ProceduralTerrain/Utilities.h
@@ -12,7 +12,24 @@ float clamp(float value, float min, float max) {
     return returner;
 }
 
+double clamp(double value, double min, double max) {
+    double returner = value;
+
+    if (value > max)
+        returner = max;
+
+    if (value < min)
+        returner = min;
+
+    return returner;
+}
+
 float lerp(float val1, float val2, float weight) {
     float nWeight = clamp(weight, 0.0f, 1.0f);
+    return (1 - nWeight) * val1 + nWeight*val2;
+}
+
+double lerp(double val1, double val2, double weight) {
+    double nWeight = clamp(weight, 0.0, 1.0);
     return (1 - nWeight) * val1 + nWeight*val2;
 }
